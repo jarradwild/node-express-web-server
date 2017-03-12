e9f5aaac79862909317c50ae962767d5ad98e962
jarradw <contact@jarradwild.com>
2017-03-12 11:16:22 +1000

    Adding a basic projects page

--- server.js
+++ server.js
@@ -40,9 +40,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-  // res.send('<h1>About Page!</h1>');
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
   });
 });
 
